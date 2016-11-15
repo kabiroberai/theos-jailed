970a0f4c249eb552f1697b63245fc4c1c71b5319
kabiroberai <oberai.kabir@gmail.com>
2016-11-15 23:13:38 +0530

    A few tiny changes

--- module/jailed.mk
+++ module/jailed.mk
@@ -1,8 +1,8 @@
 # Disable codesigning (we'll sign the dylib later)
-TARGET_CODESIGN =
+TARGET_CODESIGN ?=
 
 # Use the internal generator instead of CydiaSubstrate
-_THEOS_TARGET_DEFAULT_USE_SUBSTRATE = 0
+_THEOS_TARGET_DEFAULT_USE_SUBSTRATE := 0
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 include $(THEOS_MODULE_PATH)/jailed/rules.mk
