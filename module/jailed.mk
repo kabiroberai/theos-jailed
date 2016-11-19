d93c876fec50d310b4f09f7238c39a21b1847485
kabiroberai <oberai.kabir@gmail.com>
2016-11-19 21:35:57 +0530

    Use libc++ by default

--- module/jailed.mk
+++ module/jailed.mk
@@ -4,5 +4,8 @@ TARGET_CODESIGN =
 # Use the internal generator instead of CydiaSubstrate
 _THEOS_TARGET_DEFAULT_USE_SUBSTRATE := 0
 
+# Use libc++ by default
+_THEOS_INTERNAL_LDFLAGS += -stdlib=libc++
+
 include $(THEOS_MAKE_PATH)/tweak.mk
 include $(THEOS_MODULE_PATH)/jailed/rules.mk
