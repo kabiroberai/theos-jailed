742f9eb8dcf829179497267439ebd8e060c44bdc
kabiroberai <oberai.kabir@gmail.com>
2016-11-19 16:11:51 +0530

    Move Cycript.dylib and patchapp into their own dirs

--- module/rules.mk
+++ module/rules.mk
@@ -1,15 +1,17 @@
 .PHONY: ipa info deploy lldb
 
+PATCHAPP = $(THEOS_MODULE_PATH)/jailed/bin/patchapp
+
 _THEOS_USE_CYCRIPT := $(call __theos_bool,$(USE_CYCRIPT))
 
 ipa: all
-	$(ECHO_NOTHING)$(THEOS_MODULE_PATH)/jailed/patchapp patch "$($(TWEAK_NAME)_IPA)" "$(PROFILE)" "$(THEOS_OBJ_DIR)"/*.dylib $(_THEOS_USE_CYCRIPT)$(ECHO_END)
+	$(ECHO_NOTHING)$(PATCHAPP) patch "$($(TWEAK_NAME)_IPA)" "$(PROFILE)" "$(THEOS_OBJ_DIR)"/*.dylib $(_THEOS_USE_CYCRIPT)$(ECHO_END)
 
 info:
-	$(ECHO_NOTHING)$(THEOS_MODULE_PATH)/jailed/patchapp info "$($(TWEAK_NAME)_IPA)"$(ECHO_END)
+	$(ECHO_NOTHING)$(PATCHAPP) info "$($(TWEAK_NAME)_IPA)"$(ECHO_END)
 
 deploy:
-	$(ECHO_NOTHING)$(THEOS_MODULE_PATH)/jailed/patchapp install$(ECHO_END)
+	$(ECHO_NOTHING)$(PATCHAPP) install$(ECHO_END)
 
 lldb:
-	$(ECHO_NOTHING)$(THEOS_MODULE_PATH)/jailed/patchapp debug$(ECHO_END)
+	$(ECHO_NOTHING)$(PATCHAPP) debug$(ECHO_END)
