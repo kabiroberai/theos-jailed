3cb4b3efbfcbc3bdc24ecfb791668e6290b60c87
kabiroberai <oberai.kabir@gmail.com>
2016-11-20 11:59:20 +0530

    Improve bundle id search in patchapp

--- module/jailed.mk
+++ module/jailed.mk
@@ -7,6 +7,12 @@ _THEOS_TARGET_DEFAULT_USE_SUBSTRATE := 0
 # Use libc++ by default
 _THEOS_INTERNAL_LDFLAGS += -stdlib=libc++
 
+# Use Xcode's iOS Wildcard App ID by default
+PROFILE ?= *
+
+# Truthy USE_CYCRIPT
+_THEOS_USE_CYCRIPT := $(call __theos_bool,$(USE_CYCRIPT))
+
 ifeq ($(call __theos_bool,$(USE_FISHHOOK)),$(_THEOS_TRUE))
 $(TWEAK_NAME)_FILES += $(THEOS_MODULE_PATH)/jailed/fishhook/fishhook.c
 $(TWEAK_NAME)_CFLAGS += -I$(THEOS_MODULE_PATH)/jailed/fishhook
--- module/rules.mk
+++ module/rules.mk
@@ -2,8 +2,6 @@
 
 PATCHAPP = $(THEOS_MODULE_PATH)/jailed/bin/patchapp
 
-_THEOS_USE_CYCRIPT := $(call __theos_bool,$(USE_CYCRIPT))
-
 ipa: all
 	$(ECHO_NOTHING)$(PATCHAPP) patch "$($(TWEAK_NAME)_IPA)" "$(PROFILE)" "$(THEOS_OBJ_DIR)/$(TWEAK_NAME).dylib" $(_THEOS_USE_CYCRIPT)$(ECHO_END)
 
