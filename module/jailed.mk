fb0b96746b7d8c7d48da30c79644207856cb20a1
kabiroberai <oberai.kabir@gmail.com>
2016-11-24 20:50:45 +0530

    Improve comment explaining how to use libstdc++

--- module/jailed.mk
+++ module/jailed.mk
@@ -6,7 +6,7 @@ TARGET_CODESIGN =
 # Add jailed/lib to THEOS_LIBRARY_PATH to override stub CydiaSubstrate
 THEOS_LIBRARY_PATH += -F$(THEOS_JAILED_LIB)
 
-# Use libc++ by default - for libstdc++ use LDFLAGS += -stdlib=libstdc++
+# Use libc++ by default - for libstdc++ use TWEAK_NAME_LDFLAGS += -stdlib=libstdc++
 _THEOS_INTERNAL_LDFLAGS += -stdlib=libc++
 
 ifeq ($(call __theos_bool,$(USE_FISHHOOK)),$(_THEOS_TRUE))
