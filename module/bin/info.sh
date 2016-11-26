c805380f2e085a3c57a51f4394daf4e723b5695e
kabiroberai <oberai.kabir@gmail.com>
2016-11-26 14:06:14 +0530

    More changes to info.sh

--- module/bin/info.sh
+++ module/bin/info.sh
@@ -7,53 +7,54 @@ if [[ $? != 0 ]]; then
 	error "Failed to get entitlements for $appdir/$app_binary"
 fi
 
-function add { ent_list="$ent_list"$'\n'"$1"; }
+n=$'\n'
+function addline { ent_list="$ent_list$n$1"; }
+function add { addline ">>> $1"; } # Add heading
 
 for ent in $(grep -a '<key>' "$ENTITLEMENTS"); do
-	entitlement=$(echo $ent | cut -f2 -d\> | cut -f1 -d\<)
-	case $entitlement in
+	ent=$(echo $ent | cut -f2 -d\> | cut -f1 -d\<)
+	case $ent in
 		com.apple.developer.networking.vpn.api)
-			add ">>> VPN Configuration & Control"
+			add "VPN Configuration & Control"
 			;;
 		com.apple.developer.in-app-payments)
-			add ">>> Apple Pay (requires extra configuration)"
+			add "Apple Pay (requires extra configuration)"
 			;;
 		com.apple.external-accessory.wireless-configuration)
-			add ">>> Wireless Accessory Configuration"
+			add "Wireless Accessory Configuration"
 			;;
 		com.apple.developer.homekit)
-			add ">>> HomeKit"
+			add "HomeKit"
 			;;
 		com.apple.security.application-groups)
-			add ">>> App Groups:"
+			add "App Groups:"
 			for group in $(dd if="$ENTITLEMENTS" bs=1 skip=8 2>/dev/null | sed -ne '/application-groups/,/<\/array/p' | grep '<string>' 2>/dev/null); do
 				group_id=$(echo $group | cut -f2 -d\> | cut -f1 -d\<)-patched
-				add "    $group_id"
+				addline "    $group_id"
 			done
 			;;
 		com.apple.developer.associated-domains)
-			add ">>> Associated Domains:"
+			add "Associated Domains (requires extra configuration):"
 			for group in $(dd if="$ENTITLEMENTS" bs=1 skip=8 2>/dev/null | sed -ne '/associated-domains/,/<\/array/p' | grep '<string>' 2>/dev/null); do
 				group_id=$(echo $group | cut -f2 -d: | cut -f1 -d\<)
-				add "    $group_id"
+				addline "    $group_id"
 			done
 			;;
 		com.apple.developer.healthkit)
-			add ">>> HealthKit"
+			add "HealthKit"
 			;;
 		inter-app-audio)
-			add ">>> Inter-App Audio"
+			add "Inter-App Audio"
 			;;
 		com.apple.developer.ubiquity*)
-			add ">>> Passbook"
-			add ">>> iCloud (requires extra configuration)"
-			add ">>> Data Protection"
+			add "Passbook"
+			add "iCloud (requires extra configuration)"
+			add "Data Protection"
 			;;
 	esac
 done
 
 ent_list="$(echo "$ent_list" | sed -e :a -e '/./,$!d;/^\n*$/{$d;N;};/\n$/ba')" # Remove extra newlines
-[[ -n $ent_list ]] && ent_list=$'\n'"$ent_list"$'\n' # Add newline padding if ent_list isn't empty
+[[ -n $ent_list ]] && ent_list="$n$ent_list$n" # Add newline padding if ent_list isn't empty
 
-eval "less <<EOF
-$(<$INFO_TEMPLATE)" 2> /dev/null
+eval "less <<EOF$n$(<$INFO_TEMPLATE)"
