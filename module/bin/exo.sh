0c67fc29e9964ff3eca8df9dc9367faa7ff48f13
kabiroberai <oberai.kabir@gmail.com>
2017-01-01 18:37:49 +0530

    Improve exo packaging script

--- module/bin/exo.sh
+++ module/bin/exo.sh
@@ -5,20 +5,27 @@ source "$MESSAGES"
 rm -rf "$STAGING_DIR"
 mkdir -p "$EXO_STAGING_DIR"
 
-log 1 "Copying .dylib dependencies"
+log 1 "Copying dependencies"
 cp "$DYLIB" "$EXO_STAGING_DIR"
+
+copy_files=($EMBED_FRAMEWORKS $EMBED_LIBRARIES)
+
+for file in "${copy_files[@]}"; do
+	mkdir -p "$EXO_RESOURCES_DIR"
+	cp -a "$file" "$EXO_RESOURCES_DIR"
+done
 
 if [[ -d $RESOURCES_DIR ]]; then
 	log 1 "Copying resources"
-	mkdir "$EXO_RESOURCES_DIR"
+	mkdir -p "$EXO_RESOURCES_DIR"
 	cp -a "$RESOURCES_DIR"/ "$EXO_RESOURCES_DIR"
 fi
 
-log 2 "Merging $EXO_OUTPUT_NAME"
+log 2 "Packaging $EXO_OUTPUT_NAME"
 cd "$STAGING_DIR"
-zip -qr$COMPRESSION "$EXO_OUTPUT_NAME" "$TWEAK_NAME"
+zip -yqr$COMPRESSION "$EXO_OUTPUT_NAME" "$TWEAK_NAME"
 if [[ $? != 0 ]]; then
 	error "Failed to compress $EXO_OUTPUT_NAME"
 fi
-rm -f "$PACKAGES_DIR/$OUTPUT_NAME"
+rm -f "$PACKAGES_DIR"/*.zip
 mv "$EXO_OUTPUT_NAME" "$PACKAGES_DIR/"
